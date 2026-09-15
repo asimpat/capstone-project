5408cdcf6d0e52214a2957c9d94d1c79aeaeaccc
Asim <asimokon@yahoo.com>
2026-09-15 21:41:03 +0100

    first commit

---
+++ fastapi-backend/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="My FastAPI Backend")
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "FastAPI backend is working!"
+    }
+
+
+@app.get("/hello")
+def hello():
+    return {
+        "message": "Hello World!"
+    }
